a627a8fdf74b84f9be449d980e38730dd5129797
Nicolas <nicolas@mgig.fr>
2014-10-14 17:49:30 +0200

    Retrait fonctions spécifiques recherche
    La recherche avec mise en avant des taxonomies n’a jamais vraiment
    fonctionné… je préfère revenir à une version simple.
    
    Et puis, mieux vaut ne pas trop dépendre de SearchWP à l’avenir…

--- search.php
+++ search.php
@@ -22,53 +22,10 @@ get_header(); ?>
       
       <?php if( !empty( $query ) ) : ?>
 
-        <?php
-          $engine = SearchWP::instance();             // instatiate SearchWP
-          $supplementalEngineName = 'complete'; 	// search engine name
-         // add_filter( 'searchwp_tax_term_or_logic', '__return_true' ); // Enlever le commentaire pour recherche partielle
-
-          // perform the search
-          $posts = $engine->search( $supplementalEngineName, $query, $page );
-        ?>
 
         <?php if( !empty( $posts ) ) : ?>
           <?php foreach( $posts as $post ) : ?>
-            <?php if( $post instanceof SearchWPTermResult ) : ?>
-              <article>
-              <div class="gauche">
-
-    
-    <?php
-    echo $post->taxonomy;
-$terms = apply_filters( 'taxonomy-images-get-terms', '', array(
-'taxonomy' => $post->taxonomy,
-'term_args' => array(
-	'slug' => $post->term->slug,
-	)
-	) );
-foreach( (array) $terms as $term) {
-echo wp_get_attachment_image( $term->image_id, '' );
-}?>
-
-		
-		<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail( "archive-thumbnail" ); ?></a>
-		
-		</div>
-                  <div class="article">
-
-                <header class="entry-header"
-                		<div class="entry-content">	
-
-                  <h2 class="entry-title">
-                    <a href="<?php echo $post->link; ?>" rel="bookmark"><?php echo $post->name; ?></a>
-                  </h2>
-                		</div>
-                </header><!-- .entry-header -->
-                </div>
-                    </article>
-            <?php else : setup_postdata( $post ); ?>
-         
-              
+          
               <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <div class="article">
 	<div class="gauche">
@@ -92,7 +49,6 @@ echo wp_get_attachment_image( $term->image_id, '' );
 
 
 
-            <?php endif; ?>
           <?php endforeach; ?>
         <?php endif; ?>
 
