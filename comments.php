a885edd7de4b9b3427e2562f0daf13cc238e11c9
Nicolas <nicolas@mgig.fr>
2014-10-14 22:43:33 +0200

    Simplification commentaires
    Simplification sur deux points :
    - jamais de protection par mot de passe, donc toute cette partie est
    inutile ;
    - jamais de ping, donc retrait de la possibilité d’afficher les poings.

--- comments.php
+++ comments.php
@@ -9,22 +9,7 @@
  */
 ?>
 
-			<div id="comments">
-<?php if ( post_password_required() ) : ?>
-				<p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.', 'autofocus' ); ?></p>
-			</div><!-- #comments -->
-<?php
-		/* Stop the rest of comments.php from being processed,
-		 * but don't kill the script entirely -- we still have
-		 * to fully load the template.
-		 */
-		return;
-	endif;
-?>
-
-<?php
-	// You can start editing here -- including this comment!
-?>
+<div id="comments">
 
 <?php if ( have_comments() ) : ?>
 
@@ -48,16 +33,6 @@ foreach ( $comments as $comment )
 				<div class="nav-next"><?php next_comments_link( __( 'Commentaires plus récents <span class="meta-nav">&rarr;</span>', 'autofocus' ) ); ?></div>
 			</div><!-- .navigation -->
 		<?php endif; // check for comment navigation ?>
-
-	<?php endif; /* if ( $comment_count ) */ ?>
-
-	<?php if ( ! empty($comments_by_type['pings']) ) : ?>
-
-			<h3 id="pings-title"><?php printf($ping_count > 1 ? __('%d liens vers cet article', 'autofocus') : __('Un lien vers cet article', 'autofocus'), $ping_count) ?></h3>
-
-			<ol class="pinglist">
-				<?php wp_list_comments( array( 'callback' => 'autofocus_comment', 'type' => 'pings' ) ); ?>
-			</ol>
 
 	<?php endif; /* if ( $comment_count ) */ ?>
 
