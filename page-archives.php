648d1fc83ee10d14feae78ee15cb646baa313447
Nicolas <nicolas@mgig.fr>
2015-07-17 08:45:49 +0200

    Page des archives
    Elle ne marchait pas du tout, c’est corrigé.

--- page-archives.php
+++ page-archives.php
@@ -1,91 +1,63 @@
-<?php
+<?php get_header(); ?>
 
-get_header(); ?>
-
-		<div id="container">
+	<div id="container">
 			<div id="content" role="main">
+				<article id="post-<?php the_ID(); ?>" class="single">
+					<header class="post-header page">
+						<h2 class="post-title"><?php the_title(); ?></h2>
+					</header>
 
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<section class="post-content" style="max-width: 100%;">
+						<ul class="searchArchives">
+							<li>
+								<h3>Créateurs</h3>
+								<div class="recherche">
+									<form method="get" id="searchform"> 
+										<input class="case" type="text" value="Recherche" placeholder="Recherche" id="createur" name="createur" onblur="if (this.value == '')  {this.value = 'Recherche';}"  onfocus="if (this.value == 'Recherche') {this.value = '';}" /> 
+								  	  	<input type="hidden" id="searchsubmit" /> 
+									</form>
+								<ul class="resultats createurs_results"></ul>		
+								</div>
+							</li>
 					
-					<div class="header">
 					
-					<div class="entry-image">
-					<?php echo the_post_thumbnail('full'); ?>
-					</div>
-
-					
-						<h3 class="entry-title" style="font-size: 7em;"><?php the_title(); ?></h3>
-					</div>
-
-					<div class="entry-content">
-
-						<?php the_content(); ?>
+					<li>					
+					<h3>Acteurs</h3>
+					<div class="recherche">
+							<form method="get" id="searchform"> 
+							<input class="case" type="text" value="Recherche" placeholder="Recherche" id="acteur" name="acteur" onblur="if (this.value == '')  {this.value = 'Recherche';}"  onfocus="if (this.value == 'Recherche') {this.value = '';}" /> 
+							  <input type="hidden" id="searchsubmit" /> 
+							</form>
+							<ul id="searchPage" class="acteurs_results resultats"></ul>
+							</div>				
+						</li>	
 						
-						<div class="searchArchives">
-							
-						<li>
-							<h3>Créateurs</h3>
+						<li>	
+							<h3>Année</h3>
 							<div class="recherche">
 									<form method="get" id="searchform"> 
-									<input class="case" type="text" value="Recherche" placeholder="Recherche" id="createur" name="createur" onblur="if (this.value == '')  {this.value = 'Recherche';}"  onfocus="if (this.value == 'Recherche') {this.value = '';}" /> 
+									<input class="case" type="text" value="Recherche" placeholder="Recherche" id="annee" name="annee" onblur="if (this.value == '')  {this.value = 'Recherche';}"  onfocus="if (this.value == 'Recherche') {this.value = '';}" /> 
 									  <input type="hidden" id="searchsubmit" /> 
 									</form>
-									<ul class="resultats createurs_results"></ul>		
-							</div>
+									<ul id="searchPage" class="annees_results resultats"></ul>
+							</div>				
 						</li>
 						
 						
-						<li>					
-						<h3>Acteurs</h3>
+						<li>
+						<h3>Pays</h3>
 						<div class="recherche">
 								<form method="get" id="searchform"> 
-								<input class="case" type="text" value="Recherche" placeholder="Recherche" id="acteur" name="acteur" onblur="if (this.value == '')  {this.value = 'Recherche';}"  onfocus="if (this.value == 'Recherche') {this.value = '';}" /> 
+								<input class="case" type="text" value="Recherche" placeholder="Recherche" id="pays" name="pays" onblur="if (this.value == '')  {this.value = 'Recherche';}"  onfocus="if (this.value == 'Recherche') {this.value = '';}" /> 
 								  <input type="hidden" id="searchsubmit" /> 
 								</form>
-								<ul id="searchPage" class="acteurs_results resultats"></ul>
-								</div>				
-							</li>	
-							
-							<li>	
-								<h3>Année</h3>
-								<div class="recherche">
-										<form method="get" id="searchform"> 
-										<input class="case" type="text" value="Recherche" placeholder="Recherche" id="annee" name="annee" onblur="if (this.value == '')  {this.value = 'Recherche';}"  onfocus="if (this.value == 'Recherche') {this.value = '';}" /> 
-										  <input type="hidden" id="searchsubmit" /> 
-										</form>
-										<ul id="searchPage" class="annees_results resultats"></ul>
-								</div>				
-							</li>
-							
-							
-							<li>
-							<h3>Pays</h3>
-							<div class="recherche">
-									<form method="get" id="searchform"> 
-									<input class="case" type="text" value="Recherche" placeholder="Recherche" id="pays" name="pays" onblur="if (this.value == '')  {this.value = 'Recherche';}"  onfocus="if (this.value == 'Recherche') {this.value = '';}" /> 
-									  <input type="hidden" id="searchsubmit" /> 
-									</form>
-									<ul id="searchPage" class="pays_results resultats"></ul>
-								</div>
-						</li>
-						
-						</ul><!-- #searchArchives -->
-						
+								<ul id="searchPage" class="pays_results resultats"></ul>
+							</div>
+					</li>
 					
-					
-					
-					
-						
-						
-						<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'autofocus' ), 'after' => '</div>' ) ); ?>
-						<?php edit_post_link( __( 'Edit', 'autofocus' ), '<span class="edit-link">', '</span>' ); ?>
-					</div><!-- .entry-content -->
-				</article><!-- #post-## -->
-
-
-<?php endwhile; ?>
+					</ul><!-- #searchArchives -->
+					</section>				
+				</article>
 
 			</div><!-- #content -->
 		</div><!-- #container -->
