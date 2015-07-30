e9adc7627b6964ead683a64fbf135b4c84b705b4
Nicolas <nicolas@mgig.fr>
2015-07-30 11:07:44 +0200

    Passage à Yoast SEO
    Je voulais me passer de plugin pour la SEO et ainsi encore plus
    optimiser le thème. Mais finalement, c’est trop de boulot, et le
    résultat est bof.
    
    Donc j’ai réinstallé Yoast SEO et viré du fichier tous les ajouts
    manuels.
    
    https://wordpress.org/plugins/wordpress-seo/

--- header.php
+++ header.php
@@ -1,37 +1,5 @@
 <?php Header("Cache-Control: no-transform");?>
 
-<?php
-	if ( is_home() ) {
-		$nomPage = "À voir et à manger (Chez Nicoflo)";
-		$iMage = "http://voiretmanger.fr/blog.png";
-		$extrait = "À voir et à manger (Chez Nicoflo)";
-	
-	} else {
-	global $page, $paged;
-
-	$nomPage = wp_title( '|', false, 'right' );
-
-	// Add the blog name.
-	$nomPage = $nomPage . get_bloginfo( 'name' );
-
-	// Add a page number if necessary:
-	if ( $paged >= 2 || $page >= 2 )
-		$nomPage = ' | ' . sprintf( __( 'Page %s', 'autofocus' ), max( $paged, $page ) );
-
-	$thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full-post-thumbnail' );
-	$iMage = $thumb[0];
-
-	if ( is_single() ){
-		$post_object = get_post( $post->ID );
-		$content = $post_object->post_content;
-		$extrait = wp_trim_words( $content, $num_words = 55, $more = null );
-	}
-
-	}
-?>
-
-<!-- Début du header proprement dit -->
-
 <!DOCTYPE html>
 <head>
 
@@ -39,47 +7,13 @@
 <meta name="apple-mobile-web-app-title" content="Voir et manger" />
 <meta name="viewport" content="initial-scale=1">
 
-<meta name="description" content="<?php echo substr($extrait, 0, 160); ?>" />
-
-<meta name="twitter:card" content="summary_large_image" />
-<meta name="twitter:site" content="@nicolinuxfr" />
-<meta name="twitter:title" content="<?php echo substr($nomPage, 0, strrpos($nomPage, '|')); ?>" />
-<meta name="twitter:url" content="<?php echo home_url( $wp->request ); ?>" />
-<meta name="twitter:image:src" content="<?php echo $iMage; ?>" />
-<meta name="twitter:description" content="<?php echo $extrait; ?>" />
-
-<meta name="og:title" content="<?php echo substr($nomPage, 0, strrpos($nomPage, '|')); ?>" />
-<meta name="og:url" content="<?php echo home_url( $wp->request ); ?>" />
-<meta property="og:image" content="<?php echo $iMage; ?>" />
-<meta property="og:description" content="<?php echo $extrait; ?>" />
-
-<link rel="image_src" href="<?php echo $iMage; ?>" />
-
 <link rel="stylesheet" media="all" href="http://voiretmanger.fr/wp-content/themes/voiretmanger/style.css" />
 <link href='http://fonts.googleapis.com/css?family=Noto+Serif:400,700,400italic,700italic|Playfair+Display:400,700italic|Roboto:300,300italic,700|Dorsa' rel='stylesheet' type='text/css'>
 
- 
 <link rel="pingback" href="http://voiretmanger.fr/xmlrpc.php" />
 <link rel="shortcut icon" href="http://voiretmanger.fr/favicon.ico" />
 
-<title><?php echo $nomPage; ?></title>
-
-<script type="application/ld+json">
-{
-  "@context": "http://schema.org",
-  "@type": "WebSite",
-  "url": "http://voiretmanger.fr/",
-  "potentialAction": {
-    "@type": "SearchAction",
-    "target": "https://voiretmanger.fr?s={search_term}",
-    "query-input": "required name=search_term"
-  }
-}
-</script>
-
-<?php wp_head(); ?>
-	
-		
+<?php wp_head(); ?>	
 		
 		<?php if( get_post_meta($post->ID, 'contraste', true) ) { ?> 
 			<style>
@@ -93,15 +27,7 @@
 				}
 			</style>
 	
-	<?php } 
-
-
-	/* Always have wp_head() just before the closing </head>
-	 * tag of your theme, or you will break many plugins, which
-	 * generally use this hook to add elements to <head> such
-	 * as styles, scripts, and meta tags.
-	 */
-?>
+	<?php } ?>
 
 
 </head>
