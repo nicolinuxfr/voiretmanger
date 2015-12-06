8a1ded0263826ee936e332a85aef74aa79f688bf
Nicolas <nicolas@mgig.fr>
2015-12-06 21:07:00 +0100

    Chargement police uniquement sur les article
    Ajout d’une condition pour ne charger la police des articles que sur
    les articles

--- header.php
+++ header.php
@@ -8,8 +8,11 @@
 <meta name="viewport" content="initial-scale=1">
 
 <link rel="stylesheet" media="all" href="http://voiretmanger.fr/wp-content/themes/voiretmanger/style.css" />
-<link href='http://fonts.googleapis.com/css?family=Playfair+Display:400,700italic|Roboto:300,300italic,700|Dorsa' rel='stylesheet' type='text/css'>	
+<link href='http://fonts.googleapis.com/css?family=Playfair+Display:400,700italic|Roboto:300,300italic,700|Dorsa' rel='stylesheet' type='text/css'>
+
+<?php if (is_single()) { ?>	
 <link href='http://voiretmanger.fr/wp-content/themes/voiretmanger/css/font.css' rel='stylesheet' type='text/css'>
+<?php } ?>
 
 <link rel="pingback" href="http://voiretmanger.fr/xmlrpc.php" />
 <link rel="shortcut icon" href="http://voiretmanger.fr/favicon.ico" />
