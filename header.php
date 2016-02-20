ae05500232189602f190690c42552aceabca4773
Nicolas <nicolas@mgig.fr>
2016-02-20 10:16:12 +0100

    Ajout hreflang
    Google veut cette balise, donc…

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
 <head>
 
 <meta charset="UTF-8">
+<link rel="alternate" hreflang="fr" href="https://voiretmanger.fr" />
 <meta name="apple-mobile-web-app-title" content="Voir et manger" />
 <meta name="viewport" content="initial-scale=1">
 
