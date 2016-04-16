61b7948b956d1a5f3f0abfeaca4f6284c4605580
Nicolas <nicolas@mgig.fr>
2016-04-17 01:06:12 +0200

    Ajustements
    Va comprendre…

--- page-archives.php
+++ page-archives.php
@@ -12,51 +12,51 @@
 							<li>
 								<h3>Créateurs</h3>
 								<div class="recherche">
-									<form method="get" id="searchform"> 
-										<input class="case" type="text" value="Recherche" placeholder="Recherche" id="createur" name="createur" onblur="if (this.value == '')  {this.value = 'Recherche';}"  onfocus="if (this.value == 'Recherche') {this.value = '';}" /> 
-								  	  	<input type="hidden" id="searchsubmit" /> 
+									<form method="get" id="searchform">
+										<input class="case" type="text" value="Recherche" placeholder="Recherche" id="createur" name="createur" onblur="if (this.value == '')  {this.value = 'Recherche';}"  onfocus="if (this.value == 'Recherche') {this.value = '';}" />
+								  	  	<input type="hidden" id="searchsubmit" />
 									</form>
-								<ul class="resultats createurs_results"></ul>		
+								<ul class="resultats createurs_results"></ul>
 								</div>
 							</li>
-					
-					
-					<li>					
+
+
+					<li>
 					<h3>Acteurs</h3>
 					<div class="recherche">
-							<form method="get" id="searchform"> 
-							<input class="case" type="text" value="Recherche" placeholder="Recherche" id="acteur" name="acteur" onblur="if (this.value == '')  {this.value = 'Recherche';}"  onfocus="if (this.value == 'Recherche') {this.value = '';}" /> 
-							  <input type="hidden" id="searchsubmit" /> 
+							<form method="get" id="searchform">
+							<input class="case" type="text" value="Recherche" placeholder="Recherche" id="acteur" name="acteur" onblur="if (this.value == '')  {this.value = 'Recherche';}"  onfocus="if (this.value == 'Recherche') {this.value = '';}" />
+							  <input type="hidden" id="searchsubmit" />
 							</form>
 							<ul id="searchPage" class="acteurs_results resultats"></ul>
-							</div>				
-						</li>	
-						
-						<li>	
+							</div>
+						</li>
+
+						<li>
 							<h3>Année</h3>
 							<div class="recherche">
-									<form method="get" id="searchform"> 
-									<input class="case" type="text" value="Recherche" placeholder="Recherche" id="annee" name="annee" onblur="if (this.value == '')  {this.value = 'Recherche';}"  onfocus="if (this.value == 'Recherche') {this.value = '';}" /> 
-									  <input type="hidden" id="searchsubmit" /> 
+									<form method="get" id="searchform">
+									<input class="case" type="text" value="Recherche" placeholder="Recherche" id="annee" name="annee" onblur="if (this.value == '')  {this.value = 'Recherche';}"  onfocus="if (this.value == 'Recherche') {this.value = '';}" />
+									  <input type="hidden" id="searchsubmit" />
 									</form>
 									<ul id="searchPage" class="annees_results resultats"></ul>
-							</div>				
+							</div>
 						</li>
-						
-						
+
+
 						<li>
 						<h3>Pays</h3>
 						<div class="recherche">
-								<form method="get" id="searchform"> 
-								<input class="case" type="text" value="Recherche" placeholder="Recherche" id="pays" name="pays" onblur="if (this.value == '')  {this.value = 'Recherche';}"  onfocus="if (this.value == 'Recherche') {this.value = '';}" /> 
-								  <input type="hidden" id="searchsubmit" /> 
+								<form method="get" id="searchform">
+								<input class="case" type="text" value="Recherche" placeholder="Recherche" id="pays" name="pays" onblur="if (this.value == '')  {this.value = 'Recherche';}"  onfocus="if (this.value == 'Recherche') {this.value = '';}" />
+								  <input type="hidden" id="searchsubmit" />
 								</form>
 								<ul id="searchPage" class="pays_results resultats"></ul>
 							</div>
 					</li>
-					
+
 					</ul><!-- #searchArchives -->
-					</section>				
+					</section>
 				</article>
 
 			</div><!-- #content -->
