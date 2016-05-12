d0087e4fc7e282518ad01ef6f8212fcb84a88e83
Nicolas <nicolas@mgig.fr>
2016-05-12 22:55:21 +0200

    Gros nettoyage index.php
    Beaucoup de vieux code inutile ici :
    
    - Utilisation d’une WP_Query standard, avec des arguments utiles et
    optimisés (cf https://10up.github.io/Engineering-Best-Practices/php/)
    - Retrait de 2 div inutiles

--- index.php
+++ index.php
@@ -1,28 +1,15 @@
 <?php get_header(); ?>
+	<?php 
+	// Loop de la page d'accueil
+	$args = array (
+		'ignore_sticky_posts'    => true,
+		'update_post_meta_cache' => false,
+		'update_post_term_cache' => false,
+	);
 
-		<div id="container" class="af-layout">
-			<div id="content" role="main">
-			
-			<?php 
-			// Start the main loop 
-		
-			$wp_query = new WP_Query();
-			$wp_query->query(array(
-				'showposts' => get_option('posts_per_page'),
-				'post__not_in' => get_option('sticky_posts'),
-				'paged' => $paged
-			)); 
+	$wp_query = new WP_Query( $args );
 
-			/* 
-			 * Run the loop to output the autofocus loop.
-			 * If you want to overload this in a child theme then include a file
-			 * called loop-autofocus.php and that will be used instead.
-			 */
-			get_template_part( 'content', 'index' );
-			?>
-
-			</div><!-- #content -->
-			
-		</div><!-- #container -->
-
+	get_template_part( 'content', 'index' );
+	
+	?>
 <?php get_footer(); ?>
