32c5351d2045ec2a639d95d6267e2e33dca3588e
Nicolas F <nicolinux@gmail.com>
2017-03-19 11:48:55 +0100

    Nouvelle page d'archives
    Enfin une vraie page d’archives, avec la liste des mois et un compteur,
    rangés par année : https://voiretmanger.fr/archives/
    
    Inspiration :
    http://www.wpbeginner.com/wp-themes/how-to-customize-the-display-of-word
    press-archives-in-your-sidebar/

--- page-archives.php
+++ page-archives.php
@@ -7,8 +7,28 @@
 						<h2 class="post-title"><?php the_title(); ?></h2>
 					</header>
 
-					<section class="post-content" style="max-width: 100%;">
+					<section class="post-content archives">
 					
+					<?php
+					global $wpdb;
+					$limit = 0;
+					$year_prev = null;
+					$months = $wpdb->get_results("SELECT DISTINCT MONTH( post_date ) AS month ,	YEAR( post_date ) AS year, COUNT( id ) as post_count FROM $wpdb->posts WHERE post_status = 'publish' and post_date <= now( ) and post_type = 'post' GROUP BY month , year ORDER BY post_date DESC");
+					foreach($months as $month) :
+						$year_current = $month->year;
+						if ($year_current != $year_prev){
+							if ($year_prev != null){?>
+		
+							<?php } ?>
+						</ul>
+						<h3 class="archive-year"><a href="<?php bloginfo('url') ?>/<?php echo $month->year; ?>/"><?php echo $month->year; ?></a></h3><ul>
+	
+						<?php } ?>
+						<li><a href="<?php bloginfo('url') ?>/<?php echo $month->year; ?>/<?php echo date("m", mktime(0, 0, 0, $month->month, 1, $month->year)) ?>"><span class="archive-month"><?php echo ucfirst (date_i18n("F", mktime(0, 0, 0, $month->month, 1, $month->year))); ?> (<?php echo $month->post_count; ?>)</span></a></li>
+					<?php $year_prev = $year_current;
+
+					endforeach; ?>
+				
 					</section>
 				</article>
 
