e3e1ff070c5604592402d611160e96178c6f5b91
Nicolas <nicolas@mgig.fr>
2017-07-09 18:22:12 +0200

    Retrait des p autour des images : meilleure regex
    L’ancienne version ne fonctionnait pas avec l’éditeur de Gutengerg.
    
    Bonus : celle-ci est plus simple.

--- functions.php
+++ functions.php
@@ -216,7 +216,7 @@ add_filter('the_content_feed', 'rss_post_thumbnail');
 
 // Retirer les balises p autour des images (http://css-tricks.com/snippets/wordpress/remove-paragraph-tags-from-around-images/)
 function filter_ptags_on_images($content){
-   return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
+    return preg_replace('/<p>\\s*?(<a .*?><img.*?><\\/a>|<img.*?>)?\\s*<\\/p>/s', '\1', $content);
 }
 
 add_filter('the_content', 'filter_ptags_on_images');
