f7f4ef4bdfeb84d77e12d25457d768b722afd4fd
Nicolas <nicolas@mgig.fr>
2017-11-05 18:57:00 +0100

    Adaptations iPhone X
    Le mode paysage est désormais en plein écran sur l'iPhone X ! Avec l'encoche, et tout.
    
    Ça donne ça : https://twitter.com/nicolasfurno/status/927233225063325697

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 <meta charset="UTF-8">
 <link rel="alternate" href="<?php echo home_url( $wp->request ); ?>" hreflang="fr" />
 <meta name="apple-mobile-web-app-title" content="Voir et manger" />
-<meta name="viewport" content="initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"> 
 <meta property="fb:pages" content="384298108368" />
 
 <link href='https://voiretmanger.fr/wp-content/themes/voiretmanger/css/font.css' rel='stylesheet' type='text/css'>
