d68e1a5cc41e48cd5dd2b1f033197f96a3cc787d
Nicolas <nicolas@mgig.fr>
2018-05-18 14:20:24 +0200

    Retrait script iTunes

--- footer.php
+++ footer.php
@@ -30,10 +30,6 @@
 		$ = jQuery.noConflict();
 		$.bigfoot({actionOriginalFN: "ignore"});
 	</script>
-
-	
-	<script type='text/javascript'>var _merchantSettings=_merchantSettings || [];_merchantSettings.push(['AT', '11lwu9']);(function(){var autolink=document.createElement('script');autolink.type='text/javascript';autolink.async=true; autolink.src= ('https:' == document.location.protocol) ? 'https://autolinkmaker.itunes.apple.com/js/itunes_autolinkmaker.js' : 'http://autolinkmaker.itunes.apple.com/js/itunes_autolinkmaker.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(autolink, s);})();</script>
-
 	
 <?php } ?>
 
