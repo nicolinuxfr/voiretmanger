a92d6dff1cda47e8c6ee92f3fa73c1c34993ceef
Nicolas <nicolas@mgig.fr>
2018-08-07 18:59:23 +0200

    Ajout des mises à jour sur l'écran d'accueil de WordPress

--- functions.php
+++ functions.php
@@ -408,6 +408,29 @@ function voiretmanger_infinite_scroll_render() {
 }
 
 
+//Ajout des mises à jour sur l'écran d'accueil de WordPress (source : https://toolset.com/forums/topic/add-custom-post-type-to-dashboard-at-a-glance-box/)
+add_action( 'dashboard_glance_items', 'cpad_at_glance_content_table_end' );
+function cpad_at_glance_content_table_end() {
+    $args = array(
+        'public' => true,
+        '_builtin' => false
+    );
+    $output = 'object';
+    $operator = 'and';
+  
+    $post_types = get_post_types( $args, $output, $operator );
+    foreach ( $post_types as $post_type ) {
+        $num_posts = wp_count_posts( $post_type->name );
+        $num = number_format_i18n( $num_posts->publish );
+        $text = _n( $post_type->labels->singular_name, $post_type->labels->name, intval( $num_posts->publish ) );
+        if ( current_user_can( 'edit_posts' ) ) {
+            $output = '<a href="edit.php?post_type=' . $post_type->name . '">' . $num . ' ' . $text . '</a>';
+            echo '<li class="post-count ' . $post_type->name . '-count">' . $output . '</li>';
+        }
+    }
+}
+
+
 // FIN DES AJOUTS PERSOS
 
 /**
